79b5fc03d42f5dff5f0cd9fd18ea802ef94d8464
YLife <359059275@qq.com>
2016-12-03 22:52:15 +0800

    Interface

---
+++ src/interfaces/Employee.java
@@ -0,0 +1,48 @@
+package interfaces;
+
+public class Employee implements Comparable<Employee> {
+
+	private String name;
+	private double salary;
+	public Employee(String name,double salary){
+		this.name=name;
+		this.salary=salary;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void raiseSalary(double byPercent){
+		double raise =salary*byPercent;
+		salary+=raise;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		// TODO Auto-generated method stub
+		
+		return Double.compare(salary, other.salary);
+	}
+
+}
